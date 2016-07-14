5e04fbfa586c12634827182b23fb456cf6bd9946
Felix <finetimi@mediweightloss.com>
2016-07-14 17:18:54 -0400

    Intitial Commit

---
+++ app/components/home.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Home = React.createClass({
+	render:function(){
+		return (
+			<div> 
+				Hey I'm Home;
+			</div>
+			)
+
+	}
+
+});
+
+module.exports = Home;
